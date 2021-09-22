8245c400a9174aa878acef0ca296d83fd5d6e132
Jimmy Nordin <jimmy.nordin.1979@gmail.com>
2021-09-22 11:03:54 +0200

    Built common View for secret messages

--- src/MVC/Controllers/MVC/DevController.cs
+++ src/MVC/Controllers/MVC/DevController.cs
@@ -42,25 +42,15 @@ namespace MVC.Controllers
         }
 
 
+
         [HttpGet("AskGateway1_GetSecretForestInEurope")]
         [Authorize]
         public async Task<IActionResult> AskGateway1_GetSecretForestInEurope()
         {
-
-            var token = await HttpContext.GetTokenAsync("access_token");
-
-            //var token = await HttpContext.GetTokenAsync("access_token");
-            HttpResponseMessage httpResponseMessage = await CallURLWithAccessToken("https://localhost:44370/api/V01/Forest/GetSecretForestInEurope", token);
-            var secret = await httpResponseMessage.Content.ReadAsStringAsync();
-
-
-            //HttpResponseMessage httpResponseMessage2 = await CallURLWithAccessToken("https://localhost:44383/secret", token);
-            //var secret2 = await httpResponseMessage2.Content.ReadAsStringAsync();
-            //return View("API1Secret", new API1SecretViewModel { SecretMessage = secret, httpResponseMessage = httpResponseMessage });
-
-
-            await Task.CompletedTask;
-            return Ok(secret);
+            SecretMessageViewModel secretMessageViewModel = new();
+            secretMessageViewModel.httpResponseMessage = await CallURLWithAccessToken("https://localhost:44370/api/V01/Forest/GetSecretForestInEurope", await HttpContext.GetTokenAsync("access_token"));
+            secretMessageViewModel.SecretMessage = await secretMessageViewModel.httpResponseMessage.Content.ReadAsStringAsync();
+            return View("SecretMessage", secretMessageViewModel);
         }
 
 
